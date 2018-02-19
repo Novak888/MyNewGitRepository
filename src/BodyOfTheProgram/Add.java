ee46a21aa0ac636fd00094d2cb1cd4b474cfb990
Novak888 <Vasil4enko.alexey@yandex.ru>
2018-02-19 12:10:14 +0300

    Научил программу складывать числа

---
+++ src/BodyOfTheProgram/Add.java
@@ -0,0 +1,8 @@
+package BodyOfTheProgram;
+
+public class Add {
+    public void adding(int a, int b) {
+        int summ = a + b;
+        System.out.println("The sum is " + summ);
+    }
+}
--- src/BodyOfTheProgram/MainClass.java
+++ src/BodyOfTheProgram/MainClass.java
@@ -3,11 +3,23 @@ package BodyOfTheProgram;
 import java.util.Scanner;
 
 public class MainClass {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Please, introduce yourself.");
 
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello " + scanner.nextLine());
+
+        System.out.println("If you wont to add, enter yes.");
+        String answer = scanner.nextLine();
+        if (answer.equals("yes")) {
+            Add add = new Add();
+            System.out.println("Enter two numbers.");
+            add.adding(scanner.nextInt(), scanner.nextInt());
+        } else {
+            System.out.println("Bye!");
+        }
+
     }
 }
